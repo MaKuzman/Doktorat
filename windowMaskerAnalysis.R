5163bec2e77796ae6ebe1b5ac33c503db0542379
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2019-05-15 12:38:53 +0200

    Update windowMaskerAnalysis.R

--- windowMaskerAnalysis.R
+++ windowMaskerAnalysis.R
@@ -13,11 +13,11 @@ allranges <- sapply(x[sapply(x, length)>1], function(y){
   GRanges(y[1],IRanges(sstarts, sends))
 })
 allranges <- suppressWarnings(do.call(c,allranges))
-
+allranges <- allranges[width(allranges)>100]
 
 ### Extracting masked sequences from the genome
 library(BSgenome.Esu.UCSC.EunSub1)
-repeatsEsu <- getSeq(BSgenome.Esu.UCSC.EunSub1, allranges[width(allranges)>100])
+repeatsEsu <- getSeq(BSgenome.Esu.UCSC.EunSub1, allranges)
 LTRs <- data.frame(widths = width(LTRSequences),
                    chr = seqnames(allranges),
                    start = start(allranges),
@@ -29,11 +29,20 @@ saveRDS(LTRs, file="repeatsEsuMetadata.RDS")
 ### tSNE
 source("/common/WORK/mfabijanic/Svoboda/April2019/LTRs/Sequence_TSNE.R")
 LTRSequences <- readRDS("repeatsEsu.RDS")
-kmersinsequences <- calculate_kmers(LTRSequences, normalize=T, reverse.complement=FALSE)
 LTRs <- readRDS("repeatsEsuMetadata.RDS")
-tsne <- get_tsne(kknew,LTRs, dims=2)
-save(tsne, file="tsne.Robj")
 
-tsne5000 <- get_tsne(kknew,LTRs, dims=2, per=5000)
+kmersinsequences <- calculate_kmers(LTRSequences, normalize=T, reverse.complement=FALSE)
+kknew <- kmersinsequences[,order(apply(kmersinsequences,2,sd),decreasing=T)[1:50]]
+tsne <- get_tsne(kknew,LTRs, dims=2, per=1000)
+saveRDS(tsne, file="tsne1000.RDS")
+tsne2500 <- get_tsne(kknew,LTRs, dims=2, per=2500)
+saveRDS(tsne2500, file="tsne2500.RDS")
+
+kknew <- kmersinsequences[,order(apply(kmersinsequences,2,sd),decreasing=T)[1:1000]]
+tsne <- get_tsne(kknew,LTRs, dims=2, per=1000)
+saveRDS(tsne, file="tsne1000_first1000.RDS")
 
 
+### Plotting:
+#(same for all tsne objects)
+ggplot(tsne2500, aes(X1,X2)+geom_point()+theme_light()
