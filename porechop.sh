d44f92e3a6be571f66592cd71146e7e0fa651212
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2019-07-25 10:52:35 +0200

    Trimming the nanopores failled reads.
    /common/WORK/mfabijanic/Sponges/Assembly/Eunapius/Nanopore/Trimmed/Porechop_Fail/IndividualRuns

---
+++ porechop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -N porechopfail
+
+#PBS -m bea
+#PBS -M maja@kuzman.org
+
+#PBS -q MASTER
+#PBS -l select=1:ncpus=12:mem=100G
+
+cd $PBS_O_WORKDIR
+
+PORECHOP=/common/WORK/mfabijanic/programs/Porechop/porechop-runner.py
+
+
+for i in `ls /common/scratch/kristian/Nanopore_basecall/LSK108/workspace/fail/`
+do
+
+j=${i%%.fastq}
+k=${j##fastq_runid_}
+qsub -v i=$k -N ${k} porechopOne.sh
+done
+
